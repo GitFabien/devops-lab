5ed7b7472a5d68d37761586ae493714b09bce765
GitFabien <fabien.poplin@gmail.com>
2026-09-08 23:04:26 +0200

    initial release - python dummy app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return """
+    <h1>Hello from Docker! 🐳</h1>
+    <p>This Flask application is running inside a container.</p>
+    """
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
